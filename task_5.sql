6163a011118c2030a8871fceaa9f707d6028742c
LAPTOP-SOFGCEFF\Khater <abdelrahmann89@gmail.com>
2025-10-17 23:26:18 +0300

    added the missing semicolon

--- task_5.sql
+++ task_5.sql
@@ -49,4 +49,4 @@ CREATE TABLE IF NOT EXISTS Order_Details (
 );
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.")
+VALUES(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.");
